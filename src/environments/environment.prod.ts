3865dae1e3956bb22f7f0d8ce3e14f39ff4d0efc
Flovet-stack <flovetoloveto@yahoo.com>
2020-10-07 16:15:44 +0100

    first commit to oshop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBPmOzVaaGJZybg5d5cyBliikFlAQxK9P8",
+    authDomain: "oshop-1f60a.firebaseapp.com",
+    databaseURL: "https://oshop-1f60a.firebaseio.com",
+    projectId: "oshop-1f60a",
+    storageBucket: "oshop-1f60a.appspot.com",
+    messagingSenderId: "686752851278",
+    appId: "1:686752851278:web:6fa79e603629288c916a1f",
+    measurementId: "G-C9SJF09F2J"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBPmOzVaaGJZybg5d5cyBliikFlAQxK9P8",
+    authDomain: "oshop-1f60a.firebaseapp.com",
+    databaseURL: "https://oshop-1f60a.firebaseio.com",
+    projectId: "oshop-1f60a",
+    storageBucket: "oshop-1f60a.appspot.com",
+    messagingSenderId: "686752851278",
+    appId: "1:686752851278:web:6fa79e603629288c916a1f",
+    measurementId: "G-C9SJF09F2J"
+  }
 };
 
 /*
